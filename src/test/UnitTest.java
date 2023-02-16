ab8ebde41478de984e74932a80387b63d3e512dc
farbodomran <farbod.hashemi@tekschool.us>
2023-02-16 12:11:26 -0500

    Modified UnitTest

--- src/test/UnitTest.java
+++ src/test/UnitTest.java
@@ -5,6 +5,12 @@ public class UnitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a = 10;
+		int b = 15;
+		int c = a+b;
+		
+		System.out.println(c);
+	
 	}
 
 }
