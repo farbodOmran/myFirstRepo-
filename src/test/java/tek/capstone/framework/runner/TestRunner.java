a871d90ec279ac7508806968a82d3f06fcd77633
farbodOmran <farbod.hashemi@tekschool.us>
2023-03-02 19:26:36 -0500

    Update Capstone Project

--- src/test/java/tek/capstone/framework/runner/TestRunner.java
+++ src/test/java/tek/capstone/framework/runner/TestRunner.java
@@ -7,7 +7,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        tags = "@test",
+        tags = "@completeTest",
         features = ("classpath:features"),
         glue="tek.capstone.framework",
         dryRun = false,
